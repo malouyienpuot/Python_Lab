e2dd2cd4dad39535002cd682a6d48315ccacc3da
malouyienpuot <puotmalouyien@gmail.com>
2026-08-26 15:49:42 +0300

    Add personalized greeting feature

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from utils import celsius_to_fahrenheit, is_even, square
+from utils import celsius_to_fahrenheit, greet, is_even, square
 
 
 number = float(input("Enter a number: "))
 
+print(greet("Python student"))
 print("Square:", square(number))
 print("The number is even." if is_even(number) else "The number is odd.")
 print("Fahrenheit:", celsius_to_fahrenheit(number))
